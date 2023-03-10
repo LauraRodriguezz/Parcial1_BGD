e5312a82adeaeadd43faf89a7337db73035a2779
Ubuntu <ubuntu@ip-172-31-2-150.ec2.internal>
2023-03-10 21:16:27 +0000

    Final

--- lambda2.py
+++ lambda2.py
@@ -1,45 +1,34 @@
-import json
 import boto3
-import requests
 import datetime
 from bs4 import BeautifulSoup
 
 client = boto3.client('s3')
-BUCKET_NAME = 'parcial-bd-011'
 
-    
+
 def lambda2():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('parcial-bd-011')
     obj = bucket.Object(f"{datetime.datetime.now().strftime('%Y-%m-%d')}.html")
     body = obj.get()['Body'].read()
     soup = BeautifulSoup(body, 'html.parser')
-
     properties = soup.find_all('div', {'class': 'listing-card__information-main'})
-
     data = []
 
     for property in properties:
         price = property.find('div', {'class': 'listing-card__price-wrapper'}).text.strip()
         sqft = property.find_all('div', {'class': 'listing-card__property'})
-        sqft2 = sqft    
+        sqft2 = sqft
         casa = [price]
         for property2 in sqft2:
             casa.append(property2.text.strip())
-          
-        data.append(casa)    
-    
+        data.append(casa)
     print(data)
     s = ""
     s = s + " Precio, num_habitaciones, num_banos, metros_cuadrados\n"
     for fila in data:
         if len(fila) >= 4:
-            s = s + fila[0] + "," + fila[1].replace(".", " ") + "," + fila[2].replace(".", " ") + "," + fila[3] +"\n"
+            s = s + fila[0] + "," + fila[1].replace(".", " ") + "," + fila[2].replace(".", " ") + "," + fila[3] + "\n"
         else:
-            
             s = s + "\n"
     client = boto3.client('s3')
     client.put_object(Body=s, Bucket='parcial-bd-final-011', Key=f"{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
-
-  
-lambda2()
